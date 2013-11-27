0a9c89c4285973516b524b42ec4e1bbff243394e
elisa.castaner@citrusleaf.net <elisa.castaner@citrusleaf.net>
2013-11-27 15:10:12 -0500

    added parseMethods.py

---
+++ scripts/parseMethods.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/python
+
+import argparse
+import re
+
+def main(**kwargs):
+    f = open(kwargs['input'])
+    o = open(kwargs['output'], 'w')
+    for l in f:
+	n = ""
+	s = l.split(':')
+	if len(s[-1].lstrip().split(' '))==1:
+		print l
+		print str(s[0])+": void <init>(" + str(s[-1].lstrip().split("(")[1])
+		o.write( str(s[0])+": void <init>(" + str(s[-1].lstrip().split("(")[1]))
+	else:
+		o.write(l)
+		
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', type=str)
+    parser.add_argument('output', type=str)
+    args = parser.parse_args()
+    main(**vars(args));
