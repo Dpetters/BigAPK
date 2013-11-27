6099882b1dae954a01fe1a63cb44c44620b25a57
Dmitrij Petters <Dpetters91@gmail.com>
2013-11-27 13:22:33 -0500

    data work

--- src/AndroidInstrument.java
+++ src/AndroidInstrument.java
@@ -27,9 +27,6 @@ public class AndroidInstrument {
         //prefer Android APK files// -src-prec apk
         Options.v().set_src_prec(Options.src_prec_apk);
         
-        //output as APK, too//-f J
-        Options.v().set_output_format(Options.output_format_dex);
-        
         // resolve the PrintStream and System soot-classes
         Scene.v().addBasicClass("java.io.PrintStream",SootClass.SIGNATURES);
         Scene.v().addBasicClass("java.lang.System",SootClass.SIGNATURES);
