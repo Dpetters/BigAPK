9e9f13e32042b83d444b21376c58d8d30820328c
Dmitrij Petters <Dpetters91@gmail.com>
2013-11-27 16:30:17 -0500

    be able to run processing script by passing app name from command line

--- bin/process-apk.py
+++ bin/process-apk.py
@@ -1,13 +1,14 @@
-<target name="run" depends="compile">                                                                                
-        <java classname="AndroidInstrument">                                                                             
-            <arg value="-android-jars"/>                                                                                 
-            <arg value="android-platforms"/>                                                                             
-            <arg value="-allow-phantom-refs"/>                                                                           
-            <arg value="-process-dir"/>                                                                                  
-            <arg value="apps/apks/AlZakat.apk"/>                                                                         
-            <classpath>                                                                                                  
-                <pathelement path="lib/soot.jar"/>                                                                       
-                <pathelement path="lib/"/>                                                                               
-                <pathelement path="classes/"/>                                                                           
-            </classpath>                                                                                                 
-        </java>
+import argparse
+import subprocess
+
+def main(**kawrgs):
+    apk_path = kawrgs["apk"]
+    p = subprocess.Popen(["java", "-cp", "bin/AndroidInstrument.jar:lib/soot.jar", "AndroidInstrument", "-android-jars", "android-platforms", "-allow-phantom-refs", "-process-dir", apk_path], stdout=subprocess.PIPE)
+    print p.stdout.read()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('apk', type=str)
+    args = parser.parse_args()
+    main(**vars(args))
+
